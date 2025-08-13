ef7ced383bd918b608b2c4fa6a652a309d629fed
bubumariia <mariiakudaiberdieva@gmail.com>
2025-08-13 17:34:22 +0300

    solved leetcode problem First Unique character

---
+++ HW-W11/firstUniqueCharacter.js
@@ -0,0 +1,18 @@
+var firstUniqChar = function(s) {
+    const map = new Map()
+    
+    for(let i = 0; i < s.length; i ++){
+      if(map.has(s[i])){
+        map.set(s[i], map.get(s[i]) + 1)
+      }else{
+        map.set(s[i], 1)
+      }
+    }
+  
+  for(let j = 0; j < s.length; j++){
+    if(map.get(s[j]) == 1){
+      return j
+    }
+  }
+  return -1
+};
