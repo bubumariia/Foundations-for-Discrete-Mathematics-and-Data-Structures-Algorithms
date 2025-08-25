df2f10facd12eba9e44fc8dbbef110a960a07d9a
bubumariia <mariiakudaiberdieva@gmail.com>
2025-08-25 18:51:50 +0300

    forgot to save upsie

--- HW-W11/reverseBits.js
+++ HW-W11/reverseBits.js
@@ -1,6 +1,17 @@
+
 var reverseBits = function(n) {
     let binary = ""
-    return n
+    let output = 0
+    
+    for(let i = 0; i < 32; i++){
+      let divided = n / 2** i
+      binary += Math.floor(divided % 2)
+      
+    }
+    for( j = 0 ; j < 32; j++){
+      let multiplied = parseInt(binary[j]) * 2 ** (31 - j);
+      output += multiplied
+    }
+  return output
+   
 };
-
-console.log(reverseBits(43261596))
