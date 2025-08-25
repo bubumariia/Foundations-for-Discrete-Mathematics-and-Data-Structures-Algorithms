3b5711062b0eacd53745c3a7c46c1e3f5dc85bf4
bubumariia <mariiakudaiberdieva@gmail.com>
2025-08-25 18:52:43 +0300

    added new solution

--- HW-W11/reverseBits.js
+++ HW-W11/reverseBits.js
@@ -1,17 +1,29 @@
 
+// var reverseBits = function(n) {
+//     let binary = ""
+//     let output = 0
+    
+//     for(let i = 0; i < 32; i++){
+//       let divided = n / 2** i
+//       binary += Math.floor(divided % 2)
+      
+//     }
+//     for( j = 0 ; j < 32; j++){
+//       let multiplied = parseInt(binary[j]) * 2 ** (31 - j);
+//       output += multiplied
+//     }
+//   return output
+   
+// };
 var reverseBits = function(n) {
-    let binary = ""
     let output = 0
     
-    for(let i = 0; i < 32; i++){
-      let divided = n / 2** i
-      binary += Math.floor(divided % 2)
-      
+    for (let i = 0; i < 32; i++) {
+        let bit = Math.floor(n / 2**i) % 2;
+
+        output += bit * 2**(31 - i);
     }
-    for( j = 0 ; j < 32; j++){
-      let multiplied = parseInt(binary[j]) * 2 ** (31 - j);
-      output += multiplied
-    }
+    
   return output
    
 };
