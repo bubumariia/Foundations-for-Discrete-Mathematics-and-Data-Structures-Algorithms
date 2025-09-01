e9e723b8e573c0d2dd5b134e8585ca8f1e76154b
bubumariia <mariiakudaiberdieva@gmail.com>
2025-09-01 18:41:50 +0300

    solved leetcode problem

--- HW-W13/longestPalindrome.js
+++ HW-W13/longestPalindrome.js
@@ -1,5 +1,18 @@
 var longestPalindrome = function(s) {
-    
+    let hashMap = new Map()
+    let counter  = 0
+
+    for(let i = 0; i < s.length; i++){
+        if(hashMap.has(s[i])){
+            counter += 2
+            hashMap.delete(s[i])
+            
+        }else{
+            hashMap.set(s[i], 1)
+            
+        }
+    }
+    return s.length > counter ? counter+1: counter
 };
 
 console.log(longestPalindrome("abccccdd"))
